1819b7394ab583f815b8bc2308646da763aa2ba8
belamri-c <cld.enovation.31@gmail.com>
2025-06-25 17:44:40 +0200

    Create script.js

---
+++ assets/css/assets/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.querySelector('.nav-toggle');
+  const navLinks = document.querySelector('.nav-links');
+  toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+});
