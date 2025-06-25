582079de76179f7e471a4683d8584adda00bfcc9
belamri-c <cld.enovation.31@gmail.com>
2025-06-25 17:43:49 +0200

    Delete assets directory

--- assets/css/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.querySelector('.nav-toggle');
-  const navLinks = document.querySelector('.nav-links');
-  toggle.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-  });
-});
